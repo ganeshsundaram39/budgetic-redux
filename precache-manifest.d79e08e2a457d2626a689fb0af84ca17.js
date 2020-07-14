1d1c7ff91a87ff892932b91b2483c86afd9dc19b
ganeshsundaram39 <ganeshsundaram39@gmail.com>
2020-07-14 22:33:34 +0530

    build

---
+++ precache-manifest.d79e08e2a457d2626a689fb0af84ca17.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "782289b56e02b935d1394f839b726ef6",
+    "url": "/budgetic-redux/index.html"
+  },
+  {
+    "revision": "2f633f03a031cb784ca1",
+    "url": "/budgetic-redux/static/css/main.f6bbaf04.chunk.css"
+  },
+  {
+    "revision": "899f40a80d40e771f738",
+    "url": "/budgetic-redux/static/js/2.f5ce0a50.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/budgetic-redux/static/js/2.f5ce0a50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f633f03a031cb784ca1",
+    "url": "/budgetic-redux/static/js/main.7dbd5820.chunk.js"
+  },
+  {
+    "revision": "fb7551bb0f3c06ace76d",
+    "url": "/budgetic-redux/static/js/runtime-main.b29c1fd6.js"
+  },
+  {
+    "revision": "06d8344de7c49c0239fafc7b73bb9895",
+    "url": "/budgetic-redux/static/media/fontello.06d8344d.woff2"
+  },
+  {
+    "revision": "747400c6c6de90e4d198d5804905508f",
+    "url": "/budgetic-redux/static/media/fontello.747400c6.svg"
+  },
+  {
+    "revision": "76a7b5fd2693391ceae3ee9d1982abe2",
+    "url": "/budgetic-redux/static/media/fontello.76a7b5fd.eot"
+  },
+  {
+    "revision": "ab1e41c9684b5f05263369169c224e6b",
+    "url": "/budgetic-redux/static/media/fontello.ab1e41c9.ttf"
+  },
+  {
+    "revision": "e4f9a680802ffa5ef3e6b6953c43b2ed",
+    "url": "/budgetic-redux/static/media/fontello.e4f9a680.woff"
+  }
+]);
